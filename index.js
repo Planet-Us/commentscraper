b4f7b4261f0a0c63f6a01ccb81b55e560895fd9e
planet-us <eklee808@gmail.com>
2024-02-23 15:53:17 +0700

    changeHeader

--- index.js
+++ index.js
@@ -67,28 +67,28 @@ app.get('/getComment', async (req, res) => {
     // console.log(req);
     const result = scrapeComments(req.query.searchText, req.query.userName);
     
-    res.setHeader('Access-Control-Allow-origin', '*'); // 모든 출처(orogin)을 허용
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // 모든 HTTP 메서드 허용
-    res.setHeader('Access-Control-Allow-Credentials', 'true'); // 클라이언트와 서버 간에 쿠키 주고받기 허용
+    // res.setHeader('Access-Control-Allow-origin', '*'); // 모든 출처(orogin)을 허용
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // 모든 HTTP 메서드 허용
+    // res.setHeader('Access-Control-Allow-Credentials', 'true'); // 클라이언트와 서버 간에 쿠키 주고받기 허용
 
-    // ...
+    // // ...
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('ok');
+    // res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // res.end('ok');
     res.send(true);
 });
 
 app.get('/getCommentTemp', async (req, res) => {
     console.log(req.query);
     
-    res.setHeader('Access-Control-Allow-origin', '*'); // 모든 출처(orogin)을 허용
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // 모든 HTTP 메서드 허용
-    res.setHeader('Access-Control-Allow-Credentials', 'true'); // 클라이언트와 서버 간에 쿠키 주고받기 허용
+    // res.setHeader('Access-Control-Allow-origin', '*'); // 모든 출처(orogin)을 허용
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // 모든 HTTP 메서드 허용
+    // res.setHeader('Access-Control-Allow-Credentials', 'true'); // 클라이언트와 서버 간에 쿠키 주고받기 허용
 
-    // ...
+    // // ...
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('ok');
+    // res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // res.end('ok');
     const result = await scrapeCommentsOnlyTen(req.query.searchText);
     res.send(result);
 });
